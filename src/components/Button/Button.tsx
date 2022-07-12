9f79fb0641d69c12e65590a6267f77cdb67b16b3
Nicolas <66505477+Nicochess@users.noreply.github.com>
2022-07-12 18:28:48 -0300

    feat: simple multi step form initiate

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,13 @@
-import React, { ReactNode } from "react";
+import React, { MouseEventHandler, ReactNode } from "react";
 import { StyledButton } from "./styles";
 
 type Props = {
   children: ReactNode;
+  onClick?: MouseEventHandler
 };
 
-const Button: React.FC<Props> = ({ children }) => {
-  return <StyledButton>{children}</StyledButton>;
+const Button: React.FC<Props> = ({ children, onClick }) => {
+  return <StyledButton onClick={onClick}>{children}</StyledButton>;
 };
 
 export default Button;
--- src/screens/Register/RegisterScreen.tsx
+++ src/screens/Register/RegisterScreen.tsx
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../../components/Button";
 import Input from "../../components/Input";
 import { Container, LinkStyled } from "../../theme/stylesScreens";
 
 const RegisterScreen: React.FC = () => {
+  const [firstStep, setFirstStep] = useState<boolean>(true);
   return (
     <Container>
       <h1>My Status</h1>
-      <section>
-        <Input labelText="Username" />
-        <Input labelText="Password" />
-        <Input labelText="Confirm password" />
-        <Input labelText="Profile Picture" type="file" />
-      </section>
-      <Button>Register</Button>
+      {firstStep ? (
+        <section>
+          <Input labelText="Username" />
+          <Input labelText="Password" />
+          <Input labelText="Confirm password" />
+        </section>
+      ) : (
+        <section>
+          <Input labelText="Profile Picture" type="file" />
+          <Input labelText="Email" />
+        </section>
+      )}
+
+      <Button onClick={() => setFirstStep((prev) => !prev)}>
+        {firstStep ? "Next Step" : "Register"}
+      </Button>
       <LinkStyled to="/login">Login to your account.</LinkStyled>
     </Container>
   );
